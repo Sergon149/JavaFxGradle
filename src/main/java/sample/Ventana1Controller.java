cffb79251a6a03c6c040e97ee3cb72accce248fc
Sergio <sergon149@alu.centroafuera.es>
2021-11-25 11:01:47 +0100

    mas avanzado

--- src/main/java/sample/Ventana1Controller.java
+++ src/main/java/sample/Ventana1Controller.java
@@ -7,17 +7,41 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import org.w3c.dom.Text;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Ventana1Controller {
 
 	@FXML
 	Button b1;
+	@FXML
+	Label lTexto;
+	@FXML
+	TextArea tArea;
+	String textoOriginal = null;
+	ArrayList<String> l = new ArrayList<>();
+	int element=0;
+	@FXML
+	public void initialize(){
+		 l.add("uno");
+		 l.add("dos");
+		 l.add("tres");
+		 l.add("cuatro");
+		 l.add("cinco");
+	}
 
 	@FXML
 	private void onButtonAbrirClicked() {
 		System.out.println("Botón pulsado");
+		b1.setStyle("-fx-background-color: blue;");
+		if (element < l.size()){
+			lTexto.setText(l.get(element));
+			element++;
+		}else{
+			lTexto.setText(tArea.getText());
+		}
 	}
 	@FXML
 	private void onMouseEntered(){
